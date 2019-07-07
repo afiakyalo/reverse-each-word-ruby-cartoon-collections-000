e17b8be7e86367136dafb91bee82fb220aaac53c
Afia Kyalo <afiakyalo@gmail.com>
2019-07-07 00:20:45 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,8 @@
-def reverse_each_word("")
- a = "".split(",")
- a.each.reverse_each
- puts a
+def reverse_each_word_with_each(string)
+  original_array = string.split(" ")
+  return_array = []
+  original_array.each do|string|
+    return_array << string.reverse
+  end
+  return_array.join(" ")
 end
